872b5365543e7e9bb16b47de4aa82e4c3efbf74c
sravanp-quickcount <sravanp@quickcount.co.nz>
2022-04-02 16:37:25 +1300

    Init Commit

---
+++ app/robot.js
@@ -0,0 +1,163 @@
+class Robot {
+  constructor() {
+    this.directions = ["NORTH", "EAST", "SOUTH", "WEST"];
+    this.grid = {
+      startPointX: 0,
+      startPointY: 0,
+      lengthX: 5,
+      lengthY: 5,
+    };
+
+    this.roboCurrentPosition = {
+      x: undefined,
+      y: undefined,
+      f: undefined,
+    };
+    this.isRoboPlaced = false;
+  }
+
+  isOutOfGrid = (x, y) => {
+    if (
+      x > this.grid.startPointX + (this.grid.lengthX - 1) ||
+      x < this.grid.startPointX ||
+      y > this.grid.startPointY + (this.grid.lengthY - 1) ||
+      y < this.grid.startPointY
+    ) {
+      return true;
+    } else return false;
+  };
+
+  validate = (x, y, facing) => {
+    if (!facing) {
+      return "no Facing";
+    }
+
+    const f = facing.toUpperCase(),
+      xA = parseInt(x),
+      yA = parseInt(y);
+
+    if (xA < 0 || yA < 0) {
+      return "no Facing";
+    }
+
+    if (!this.isDirectionValid(f)) {
+      return "In Valid Direction";
+    }
+
+    return {
+      x: xA,
+      y: yA,
+      f,
+    };
+  };
+
+  isDirectionValid = (face) => {
+    return this.directions.indexOf(face) !== -1;
+  };
+
+  setPosition = function (x, y, f) {
+    (this.roboCurrentPosition.x = x),
+      (this.roboCurrentPosition.y = y),
+      (this.roboCurrentPosition.f = this.directions.indexOf(f));
+  };
+
+  getPosition = () => {
+    return {
+      x: this.roboCurrentPosition.x,
+      y: this.roboCurrentPosition.y,
+      f: this.directions[this.roboCurrentPosition.f],
+    };
+  };
+
+  place = (x, y, f) => {
+    console.log(x, y, f);
+    let arg = {};
+
+    try {
+      arg = this.validate(x, y, f);
+    } catch (e) {
+      return e;
+    }
+
+    if (this.isOutOfGrid(arg.x, arg.y)) {
+      return "Robot moving out of grid give the correct command";
+    }
+
+    this.setPosition(arg.x, arg.y, arg.f);
+
+    if (!this.isRoboPlaced) this.isRoboPlaced = true;
+
+    return this;
+  };
+
+  move = () => {
+    let x, y, f;
+
+    if (!this.isRoboPlaced) {
+      return "PLACE the robot first";
+    }
+
+    x = this.roboCurrentPosition.x;
+    y = this.roboCurrentPosition.y;
+    f = this.roboCurrentPosition.f;
+
+    switch (f) {
+      case 0: // NORTH
+        ++y;
+        break;
+      case 1: // EAST
+        ++x;
+        break;
+      case 2: // SOUTH
+        --y;
+        break;
+      case 3: // WEST
+        --x;
+        break;
+    }
+
+    if (this.isOutOfGrid(x, y)) {
+      return "Robot moving out of grid give the correct command";
+    }
+
+    this.setPosition(x, y, this.directions[f]);
+
+    return this;
+  };
+
+  right = () => {
+    if (!this.isRoboPlaced) {
+      return "PLACE the robot first";
+    }
+    this.roboCurrentPosition.f =
+      this.roboCurrentPosition.f + 1 > 3 ? 0 : this.roboCurrentPosition.f + 1;
+    return this;
+  };
+
+  left = () => {
+    if (!this.isRoboPlaced) {
+      return "PLACE the robot first";
+    }
+    this.roboCurrentPosition.f =
+      this.roboCurrentPosition.f - 1 < 0 ? 3 : this.roboCurrentPosition.f - 1;
+    return this;
+  };
+
+  report = () => {
+    const currentPos = this.getPosition();
+
+    if (
+      currentPos.x == undefined &&
+      currentPos.y == undefined &&
+      currentPos.f == undefined
+    ) {
+      return "PLACE the robot first";
+    } else {
+      return (
+        "Output: " + currentPos.x + "," + currentPos.y + "," + currentPos.f
+      );
+    }
+  };
+};
+
+module.exports = new Robot();
---
+++ index.js
@@ -0,0 +1,97 @@
+const Robot = require("./app/robot.js");
+const os = require("os"),
+  stdin = process.stdin,
+  stdout = process.stdout,
+  stderr = process.stderr,
+  EOL = os.EOL,
+  fs = require("fs"),
+  readline = require("readline");
+dotenv = require("dotenv");
+dotenv.config();
+let rl, argv;
+
+stdin.setEncoding("utf8");
+
+argv = process.argv.slice(2);
+
+// read stdin
+stdin.on("data", (data) => {
+  printMsg(data);
+});
+
+if (argv.length) {
+  try {
+    fs.accessSync(argv[0], fs.F_OK | fs.R_OK);
+  } catch (e) {
+    stderr.write(`file path not found ${argv[0]}`);
+    process.exit();
+  }
+
+  stderr.write(`Reading commands from ${argv[0]}` + EOL);
+
+  rl = readline.createInterface({
+    input: fs.createReadStream(argv[0]),
+    terminal: false,
+  });
+
+  rl.on("line", (line) => {
+    stdout.write(line + EOL);
+    printMsg(line);
+  });
+
+  rl.on("close", () => {
+    rl.close();
+    process.exit();
+  });
+}
+
+const executeCommand = (order) => {
+  let res;
+  if (order.match(/^\s*place\s+\w+(?:,?\s*|\s+)\w+(?:,?\s*|\s+)\w+\s*$/i)) {
+    const params = order
+      .trim()
+      .split(/(?:\s+|,\s*)/i)
+      .slice(1);
+    res = Robot.place(params[0], params[1], params[2]);
+  } else if (order.match(/^move\s*$/i)) {
+    res = Robot.move();
+  } else if (order.match(/^left\s*$/i)) {
+    res = Robot.left();
+  } else if (order.match(/^right\s*$/i)) {
+    res = Robot.right();
+  } else if (order.match(/^report\s*$/i)) {
+    res = Robot.report();
+  } else {
+    res = "Invalid Command";
+  }
+  return res;
+};
+
+const printMsg = (data) => {
+  let res;
+  const cmd = data.trim();
+
+  if (cmd.match(/(q|quit|exit)/i)) process.exit();
+
+  res = executeCommand(cmd);
+
+  if (res instanceof Error) {
+    stdout.write(`${res.message + EOL}> `);
+  } else if (typeof res == "string") {
+    stdout.write(`${res + EOL}> `);
+  } else {
+    stdout.write("> ");
+  }
+};
+
+const App = () => {};
+
+App.run = () => {
+  stdout.write(
+    `Hola! ${EOL} Start by Giving the Command - PLACE X, Y, F Eg: 1,1, South.` +
+      EOL
+  );
+  stdin.resume();
+};
+
+App.run();
